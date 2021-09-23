0f8063aa4fe3d45fd28c35c56bd4f44aab61bb6f
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-09-24 02:22:06 +0500

    added vuex-persistedstate package for faster web

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
@@ -7,5 +8,6 @@ export default new Vuex.Store({
 	state: {},
 	mutations: {},
 	actions: {},
+	plugins: [createPersistedState()],
 	modules: {},
 });
