a71d6f0351630b298c31c3298f1458c92e20b7ca
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2021-09-24 02:26:32 +0500

    added store example

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,9 +5,27 @@ import createPersistedState from 'vuex-persistedstate';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
+	state: {
+		user: {},
+	},
+	mutations: {
+		SET_USER(state, user) {
+			state.user = user;
+		},
+	},
+	actions: {
+		fetchUser({ commit }) {
+			axios
+				.get('/auth/user')
+				.then(res => {
+					commit('SET_USER', res.data);
+					return res.data;
+				})
+				.catch(err => {
+					return err;
+				});
+		},
+	},
 	plugins: [createPersistedState()],
 	modules: {},
 });
